49105c6b031abe70b6fca42c3a80f4cec04b32f3
Roman <kolesnikmesser8959@gmail.com>
2025-04-28 19:45:49 +0200

    Version 1.1

--- front-post.php
+++ front-post.php
@@ -1,27 +1,31 @@
 <?php
 
 /**
- * Front Post
- * @package           Front Post
- * @author            WPCat
- * Plugin Name:       Front Post
- * Description:       Creating posts in frontend
- * Version:           1.0
+ * Plugin Name: Art Frontend Create Posts
+ * Plugin URI: wpruse.ru
+ * Text Domain:
+ * Domain Path: /languages
+ * Description: Плагин добавления записей с фронта
+ * Version: 1.0.0
+ * Author: Artem Abramovich
+ * Author URI: https://wpruse.ru/
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
  */
 
- if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
-}
-define('FP_DIR', plugin_dir_path(__FILE__));
-define('FP_URI', plugin_dir_url(__FILE__));
-
-require FP_DIR . 'includes\class-fp-core.php';
-
-// error_log(print_r(FP_DIR, 1));
-// error_log(print_r(FP_URI, 1));
-
-function fp(){
-    return FP_Core::instance();
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-fp();
+define( 'AFCP_DIR', plugin_dir_path( __FILE__ ) );
+define( 'AFCP_URI', plugin_dir_url( __FILE__ ) );
+
+require AFCP_DIR . 'includes/class-afcp-core.php';
+
+function afcp(){
+
+    return AFCP_Core::instance();
+}
+
+afcp();
