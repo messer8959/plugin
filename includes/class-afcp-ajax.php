cc85be375a20b19fddd9b56a8f3afecba0264667
Roman <kolesnikmesser8959@gmail.com>
2025-04-30 20:14:59 +0200

    Silmle Fields Validation

--- includes/class-afcp-ajax.php
+++ includes/class-afcp-ajax.php
@@ -8,6 +8,54 @@ class AFCP_Ajax{
 
     public function callback(){
         error_log( print_r( $_POST, 1) );
+
+        check_ajax_referer( 'afcp-ajax-nonce', 'nonce' );
+
+        $this->validation();
+
+
         wp_die();
+    }
+
+    public function validation(){
+        $error = [];
+
+        $required = [
+            'event_title'        => 'This is required field',
+            // 'event_topics'       => 'This is required field',
+            // 'event_hashtags'     => 'This is required field',
+            // 'event_descriptions' => 'This is required field',
+            // 'event_thumbnail'    => 'This is required field',
+            // 'event_date'         => 'This is required field',
+            // 'event_location'     => 'This is required field'
+        ];
+
+        foreach( $required as $key => $item ) {
+            if( empty( $_POST[$key] ) || ! isset( $_POST[$key] ) ){
+                $error[ $key ] = $item;
+            }
+        }
+
+        if( $error ){
+            $this->error($error );
+        }
+    }
+
+    public function success($message){
+        
+        wp_send_json_success([
+            'response' => 'SUCCESS',
+            'message' => $message
+        ]);
+
+    }
+
+    public function error($message){
+        
+        wp_send_json_error([
+            'response' => 'ERROR',
+            'message' => $message
+        ]);
+
     }
 }
