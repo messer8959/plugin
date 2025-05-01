9d6b1f80d8d27c23c70eddb356729680e9b6cadc
Roman <kolesnikmesser8959@gmail.com>
2025-05-01 19:16:29 +0200

    Post Creating Successful

--- includes/class-afcp-ajax.php
+++ includes/class-afcp-ajax.php
@@ -1,90 +1,111 @@
 <?php
 
-class AFCP_Ajax{
-    public function __construct(){
-        add_action( 'wp_ajax_created_event', [ $this, 'callback' ] );
-		add_action( 'wp_ajax_nopriv_created_event', [ $this, 'callback' ] );
+class AFCP_Ajax
+{
+    public function __construct()
+    {
+        add_action('wp_ajax_created_event', [$this, 'callback']);
+        add_action('wp_ajax_nopriv_created_event', [$this, 'callback']);
     }
 
-    public function callback(){
-        error_log( print_r( $_FILES, 1) );
+    public function callback()
+    {
+        error_log(print_r($_POST, 1));
 
-        check_ajax_referer( 'afcp-ajax-nonce', 'nonce' );
+        check_ajax_referer('afcp-ajax-nonce', 'nonce');
 
         $this->validation();
+
         $this->validation_thumbnail();
 
+        $event_data = [
+            'post_type' => 'event',
+            'post_status' => 'draft',
+            'post_title' => sanitize_text_field($_POST['event_title']),
+            'post_content' => wp_kses_post($_POST['event_descriptions']),
+            'meta_input' => [
+                'event_date' => sanitize_text_field($_POST['event_date']),
+                'event_location' => sanitize_text_field($_POST['event_location']),
+            ],
+            'tax_input' => [
+                'topics' => sanitize_text_field($_POST['event_topics'])
+            ]
+        ];
+
+        wp_insert_post($event_data);
 
         wp_die();
     }
 
-    public function validation(){
+    public function validation()
+    {
         $error = [];
 
         $required = [
-            'event_title'        => 'This is required field',
-            'event_topics'       => 'This is required field',
+            // 'event_title'        => 'This is required field',
+            // 'event_topics'       => 'This is required field',
             // 'event_hashtags'     => 'This is required field',
             // 'event_descriptions' => 'This is required field',
             // 'event_thumbnail'    => 'This is required field',
-            'event_date'         => 'This is required field',
-            'event_location'     => 'This is required field'
+            // 'event_date'         => 'This is required field',
+            // 'event_location'     => 'This is required field'
         ];
 
-        foreach( $required as $key => $item ) {
-            if( empty( $_POST[$key] ) || ! isset( $_POST[$key] ) ){
-                $error[ $key ] = $item;
+        foreach ($required as $key => $item) {
+            if (empty($_POST[$key]) || ! isset($_POST[$key])) {
+                $error[$key] = $item;
             }
         }
 
-        if( $error ){
-            $this->error($error );
+        if ($error) {
+            $this->error($error);
         }
     }
 
-    public function validation_thumbnail(){
+    public function validation_thumbnail()
+    {
 
-        $size = getimagesize($_FILES['event_thumbnail']['tmp_name']); 
+        $size = getimagesize($_FILES['event_thumbnail']['tmp_name']);
         $max_size = 800;
-        $type = $_FILES['event_thumbnail']['type'];
+        // $type = $_FILES['event_thumbnail']['type'];
 
 
-        if( $size[0] > $max_size || $size[1] > $max_size){
+        if ($size[0] > $max_size || $size[1] > $max_size) {
 
             unlink(($_FILES['event_thumbnail']['tmp_name']));
 
             $image_message = 'Unacceptable Imega Size';
 
             $this->error($image_message);
+        }
 
-        };
+        // if( 'image/jpeg' !== $type || 'image/png' !== $type ){
 
-        if( 'image/jpeg' !== $type || 'image/png' !== $type){
+        //     unlink(($_FILES['event_thumbnail']['tmp_name']));
 
-            unlink(($_FILES['event_thumbnail']['tmp_name']));
+        //     $image_message = 'Unacceptable Image Format';
 
-            $image_message = 'Unacceptable Imega Format';
+        //     $this->error($image_message);
 
-            $this->error($image_message);
+        // }
 
-        };
     }
 
-    public function success($message){
-        
+    public function success($message)
+    {
+
         wp_send_json_success([
             'response' => 'SUCCESS',
             'message' => $message
         ]);
-
     }
 
-    public function error($message){
-        
+    public function error($message)
+    {
+
         wp_send_json_error([
             'response' => 'ERROR',
             'message' => $message
         ]);
-
     }
 }
